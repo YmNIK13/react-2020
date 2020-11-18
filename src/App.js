5c384b44d9dc81680318a762b4a6e9c39683f692
YmNIK13 <Zyfnt,trfryfd01yt>
2020-11-18 20:18:20 +0200

    add loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
     // используем для хранения состояния
-    const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: 'купить хлеб'},
-        {id: 2, completed: true, title: 'купить масло'},
-        {id: 3, completed: false, title: 'купить молоко'},
-    ])
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    // вторым параметром передаем второй массив зависимостей
+    // так как у нас таких зависимостей нет то передаем пустой массив
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(json => {
+                setTimeout(() => {
+                    setTodos(json)
+                    setLoading(false)
+                }, 2000)
+            })
+    }, [])
 
     function toggleTodo(id_item) {
         setTodos(todos.map(todo => {
@@ -39,9 +50,12 @@ function App() {
             <div className='wrapper'>
                 <h1>React tutorial</h1>
                 <AddTodo onCreate={addTodo}/>
-                {todos.length > 0
-                    ? (<TodoList todos={todos} onToggle={toggleTodo}/>)
-                    : (<p>No todos!</p>)
+                {
+                    loading
+                        ? <Loader/>
+                        : todos.length > 0
+                        ? (<TodoList todos={todos} onToggle={toggleTodo}/>)
+                        : (<p>No todos!</p>)
                 }
             </div>
         </Context.Provider>
---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => <div style={{display: 'flex', justifyContent: 'canter', margin: '.5rem'}}>
+    <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
